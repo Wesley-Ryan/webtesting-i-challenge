317e5400827866dc5ab81b39f349fee4fd49a0a2
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-12-16 13:11:23 -0800

    Passing item requirement test

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,4 +1,13 @@
+class Item {
+  constructor(name) {
+    this.name = name;
+    this.durability = 0;
+    this.enhancement = 0;
+  }
+}
+
 module.exports = {
+  Item,
   success,
   fail,
   repair,
@@ -6,27 +15,21 @@ module.exports = {
 };
 
 function success(item) {
+  // a success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success.
   return { ...item };
 }
 
 function fail(item) {
+  // a fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
   return { ...item };
 }
 
 function repair(item) {
+  // a repair(item) method that accepts an item object and returns a new item with the durability restored to 100. This method is the simplest of the three and would be a good starting point on this project.
   return { ...item };
 }
 
 function get(item) {
+  // a get() method for use when working on the stretch problem.
   return { ...item };
 }
-
-// a repair(item) method that accepts an item object and returns a new item with the durability restored to 100. This method is the simplest of the three and would be a good starting point on this project.
-
-// a success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success.
-
-// a fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
-
-// a get() method for use when working on the stretch problem.
-
-// The following sections list information provided by the client about items and the game's enhancing system.
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,27 @@
 const enhancer = require("./enhancer.js");
 // test away!
-
+describe("Sanity check", () => {
+  it("works", () => {
+    // THIS is the test
+    // assertions
+    expect(2 + 2).toBe(4);
+  });
+});
 // Tests
+
+describe("Verify Item Requirements", () => {
+  const itemCreator = enhancer.Item;
+  let item;
+  beforeEach(() => {
+    item = new itemCreator("milk");
+  });
+  it("Item can be created and named", () => {
+    expect(item.name).toBe("milk");
+  });
+  it("Item has name, durability, enhancement", () => {
+    expect(item).toMatchObject({ name: "milk", durability: 0, enhancement: 0 });
+  });
+});
 
 // Items have name, durability and enhancement.
 // The item's enhancement it's a number from 0 to 20.
@@ -17,4 +37,4 @@ const enhancer = require("./enhancer.js");
 
 // If the item's enhancement is less than 15, the durability of the item is decreased by 5.
 // If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
-// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17)
